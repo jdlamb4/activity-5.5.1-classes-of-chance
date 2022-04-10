a4e6269f9e274f8188133f35fe40dc6c2ebd8885
Jacob Lamb <jacoblamb15@gmail.com>
2022-04-10 11:02:48 -0400

    activity done

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 class Casino {
-    // Write code here
+    constructor(name, timesPlayed, fakeCoin){
+        this.name = name;
+        this.timesPlayed = 0;
+        this.fakeCoin = fakeCoin;
+    }
+
+    playGame(betAmount) {
+        this.timesPlayed += 1;
+        if (Math.random() > 0.5 && this.fakeCoin == false) {
+            console.log("You won " + betAmount * (this.timesPlayed) + " dollard from " + this.name + "!");
+            this.winnings += this.timesPlayed * betAmount;
+            console.log("Current Winnings: " + this.winnings);
+        } 
+        
+        else {
+            console.log(this.name + " wins!");
+            this.timesPlayed = 0;
+            this.winnings = 0;
+        }
+    }
+    
+    rollDice(sides) {
+        console.log(Math.floor(Math.random() * sides) + 1);
+    }
+
+
 };
 
 // TESTS
